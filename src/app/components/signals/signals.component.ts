e193a80df1860b1bfe6c086cfc1c6cd70ef10bdd
daryia_kostenok <d.kastsianok@gmail.com>
2023-11-16 19:36:02 +0100

    feat: add signals component

---
+++ src/app/components/signals/signals.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-signals',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './signals.component.html',
+  styleUrl: './signals.component.scss'
+})
+export class SignalsComponent {
+
+}
